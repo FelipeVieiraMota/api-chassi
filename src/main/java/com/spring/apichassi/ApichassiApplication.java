acd9261ab7559398b5bbfe24a8132e4e41b3bc48
Felipe Vieira Mota <felipevieiramota@gmail.com>
2021-02-10 18:19:45 -0300

    Firs Commit

---
+++ src/main/java/com/spring/apichassi/ApichassiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.apichassi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApichassiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApichassiApplication.class, args);
+	}
+
+}
